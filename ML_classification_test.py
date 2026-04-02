374307584bfa111e5f09735e201ce02a4c1f217d
SamaToxich <gowalisin.333@gmail.com>
2026-04-02 16:56:15 +0400

    update

--- ML_classification_test.py
+++ ML_classification_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from xgboost import XGBClassifier
@@ -16,13 +17,8 @@ print(df.describe())
 
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42, stratify=y)
 
-model = XGBClassifier(
-    n_estimators=100,
-    max_depth=5,
-    learning_rate=0.1,
-    random_state=42,
-    eval_metric="logloss"
-)
+model = KNeighborsClassifier()
+#model = XGBClassifier(n_estimators=100, max_depth=5, learning_rate=0.1, eval_metric="logloss")
 
 model.fit(x_train, y_train)
 
